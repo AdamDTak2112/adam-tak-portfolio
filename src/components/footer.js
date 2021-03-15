adc0bafcc9e116daca366b05383f928c4dd0e0ff
Adam Tak <adam.tak2112@gmail.com>
2021-03-15 16:54:57 -0600

    implement dynamic nav menu for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { footerMenuData } from "../data/footerMenuData"
 
 const Footer = ({ data }) => {
   return (
-    //TODO: Implement dynamic menu based on footerMenuData
     <Nav>
-      <NavLink to="https://github.com/AdamDTak2112">Github!</NavLink>
-      <NavLink to="/contact">Contact Me!</NavLink>
+      <NavMenu>
+        {footerMenuData.map((item, index) => (
+          <NavLink to={item.link} key={index}>
+            {item.title}
+          </NavLink>
+        ))}
+      </NavMenu>
     </Nav>
   )
 }
 
 export default Footer
 
+const FooterText = styled.p``
+
 const Nav = styled.nav`
   background: black;
   height: 40px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   padding: 0.5rem calc((100vw - 1300px) / 2);
   z-index: 100;
   position: fixed;
@@ -35,4 +42,11 @@ const NavLink = styled(Link)`
   padding: 0 1rem;
   height: 0%;
   cursor: pointer;
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  margin-right: -40px;
 `
